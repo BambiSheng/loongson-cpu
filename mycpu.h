0c8c14fb2b7d83a21439c0efcab4744de957b3fd
bambi <bambi425sl@gmail.com>
2024-07-21 16:10:11 +0800

    csr.v init

--- mycpu.h
+++ mycpu.h
@@ -4,4 +4,18 @@
     `define IF_ID_LEN 64
     `define ID_EX_LEN 162
 
+
+    `define CRMD 14'h0
+    `define PRMD 14'h1
+    `define ECFG 14'h4
+    `define ESTAT 14'h5
+    `define ERA 14'h6
+    `define EENTRY 14'hc
+    `define SAVE0  14'h30
+    `define SAVE1  14'h31
+    `define SAVE2  14'h32
+    `define SAVE3  14'h33
+
+
+
 `endif
